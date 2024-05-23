14fa7300b18c7f4ce33559476f4b94759da7516f
wilfred11 <wilfredvancasteren@hotmail.com>
2024-05-23 17:55:15 +0200

    webpack

--- src/djapy_app/pages/dt/families.js
+++ src/djapy_app/pages/dt/families.js
@@ -1,1 +1,3 @@
 console.log('test');
+alert('ok');
+console.log('rt');
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,14 +1,38 @@
 const { merge } = require('webpack-merge');
-const {configBasic, configDataTable, config_} = require('./webpack.config.js')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {configBasic, configDataTable, config_, config_dt_} = require('./webpack.config.js')
 
 module.exports = [merge(configBasic, {
     mode: 'production',
-    // ... (environment-specific settings)
-}), merge(configDataTable, {
+
+    output: {
+        filename: "[name].[contenthash].js",
+    },
+    }), merge(configDataTable, {
     mode: 'production',
-    // ... (environment-specific settings)
-}),merge(config_, {
+    output: {
+         filename: "[name]-dt.[contenthash].js",
+    },
+    }),merge(config_, {
     mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js',
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '../css/index.[contenthash].css',
+        })
+    ],
+    }),merge(config_dt_, {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js',
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '../css/index-dt.[contenthash].css',
+        })
+    ]
     // ... (environment-specific settings)
 })
 ]
